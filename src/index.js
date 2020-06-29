cdd0ba67fafdb00b6489327850e4286eb21b5237
priest4612 <prie4612@gmail.com>
2020-06-29 19:19:43 +0300

    Подключил пропсы

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './app/app.jsx';
+import App from './components/app/app.jsx';
+import filmsMock from './mock/films-mock.json';
+import genresMock from './mock/genres-mock.json';
 
-const moveDetails = {
-  title: `The Grand Budapest Hotel`,
-  genre: `Drama`,
-  year: 2014,
-};
+
+const moveDetails = filmsMock[17];
+const movies = filmsMock;
+const genres = genresMock;
 
 ReactDOM.render(
     <App
       moveDetails={moveDetails}
+      movies={movies}
+      genres={genres}
     />,
     document.querySelector(`#root`)
 );
