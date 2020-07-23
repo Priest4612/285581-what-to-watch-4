4a9af177ef8ba33eae1f79ac76e79b7f27d53908
priest4612 <prie4612@gmail.com>
2020-07-23 21:35:33 +0300

    Обновил тесты

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+import moviesMocks from '../../mocks/test-films-mock.json';
+import genresMocks from '../../mocks/test-genres-mock.json';
 
 import App from './app.jsx';
 
+const mockStore = configureStore([]);
+const store = mockStore({
+  genres: {
+    list: genresMocks,
+  },
+  movies: {
+    list: moviesMocks,
+  }
+});
 
 it(`Render App`, () => {
   const tree = renderer
-    .create(<App />)
+    .create(
+        <Provider store={store}>
+          <App
+          />
+        </Provider>
+    )
     .toJSON();
 
   expect(tree).toMatchSnapshot();
